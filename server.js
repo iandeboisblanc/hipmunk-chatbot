3ac3e0d9e31187918c50565f7849da30620fc942
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-10-16 14:15:00 -0700

    Add library to parse requests

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const formidable = require('express-formidable');
+
 const PORT = process.env.npm_package_config_port;
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello World!')
+// Allow cross-origin requests
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
+// Handle parsing of multipart forms
+server.use(formidable());
+
+server.post('/chat/messages', (req, res) => {
+  console.log('Got message!');
+  console.log(req.fields)
+})
+
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}.`);
 });
