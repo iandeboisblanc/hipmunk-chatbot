f72d9703cc204965a1c7465c1e0736c269820883
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-10-16 14:18:50 -0700

    Restrict cross-origin requests to hipmunk.github.io

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 
 // Allow cross-origin requests
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://hipmunk.github.io');
   next();
 });
 
