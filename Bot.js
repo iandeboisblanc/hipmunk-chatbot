ba37f91544a028e0f5aa6762ae24442bc4487c12
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-10-17 12:41:08 -0700

    Add regex parsing to get location

--- Bot.js
+++ Bot.js
@@ -4,7 +4,6 @@ const config = require('./config.json');
 class Bot {
 
   async generateResponse(message) {
-    console.log(message);
     try {
       switch (message.action) {
         case 'join':
@@ -13,13 +12,8 @@ class Bot {
           return await this.handleMessageAction(message);
       }
     } catch(e) {
-      console.error(e);
-      return {
-        messages: [{
-          type: 'text',
-          text: 'I\'m sorry, something went wrong. Please try again!'
-        }]
-      };
+      console.error(`Error generating response: "${e}"`);
+      return this.defaultReply('error');
     }
   }
 
@@ -32,41 +26,21 @@ class Bot {
     };
   }
 
-  getLatLng(location) {
-    const queryParams = `address=${location}&key=${config.GMAPS_API_KEY}`;
-    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?${queryParams}`)
-      .then(res => res.json())
-      .then(body => {
-        if (body.status !== 'OK') {
-          return Promise.reject(body.error_message || 'Unknown error')
-        }
-        console.log('Got result from gmaps:', body);
-        return body.results[0].geometry.location;
-      }).catch(err => {
-        console.error(`Error getting location from GMAPS: "${err}"`);
-        return null;
-      });
-  }
-
-  getWeather({ lat, lng }) {
-    const queryParams = 'exclude=minutely,hourly'
-    return fetch(`https://api.darksky.net/forecast/${config.DARK_SKY_API_KEY}/${lat},${lng}?${queryParams}`)
-      .then(res => res.json())
-      .then(body => {
-        console.log('Got results from darksky:', body)
-        return body.currently;
-      }).catch(err => {
-        console.error(`Error getting weather from Dark Sky: "${err}"`);
-        return null;
-      });
-  }
-
   async handleMessageAction(message) {
-    // TODO: Decide if weather
-    const latLng = await this.getLatLng('San Francisco');
-    if (!latLng) { return this.defaultReply() }
-    const weather = await this.getWeather(latLng);
-    if (!weather) { return this.defaultReply() }
+    // Match phrase like: (What's) (the weather) (in) <Location>
+    const sentenceRegex = /(?:whats|what is|hows|what's|how's|how is)?\s*(?:weather|the weather)\s+(?:of|in)?\s?(.*)$/;
+    // Match phrase like: <Location> (weather)
+    const clauseRegex = /(.*)(?=\s+(weather|the weather))/;
+
+    const lowerCaseText = message.text.toLowerCase();
+    const location = lowerCaseText.match(sentenceRegex) || lowerCaseText.match(clauseRegex);
+
+    if (!location) { return this.defaultReply() }
+
+    const latLng = await this._getLatLng(location);
+    if (!latLng) { return this.defaultReply('weather') }
+    const weather = await this._getWeather(latLng);
+    if (!weather) { return this.defaultReply('weather') }
     return {
       messages: [{
         type: 'text',
@@ -75,13 +49,57 @@ class Bot {
     };
   }
 
-  defaultReply() {
-    return {
-      messages: [{
-          type: 'text',
-          text: 'I\m sorry, I don\'t know how to help with that. Try asking me something like "What\'s the weather in Chicago?"'
-      }]
-    };
+  _getLatLng(location) {
+    const queryParams = `address=${location}&key=${config.GMAPS_API_KEY}`;
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?${queryParams}`)
+    .then(res => res.json())
+    .then(body => {
+      if (body.status !== 'OK') {
+        return Promise.reject(`${body.status}--${body.error_message}`)
+      }
+      return body.results[0].geometry.location;
+    }).catch(err => {
+      console.error(`Error getting location from GMAPS: "${err}"`);
+      return null;
+    });
+  }
+
+  _getWeather({ lat, lng }) {
+    const queryParams = 'exclude=minutely,hourly'
+    return fetch(`https://api.darksky.net/forecast/${config.DARK_SKY_API_KEY}/${lat},${lng}?${queryParams}`)
+    .then(res => res.json())
+    .then(body => {
+      return body.currently;
+    }).catch(err => {
+      console.error(`Error getting weather from Dark Sky: "${err}"`);
+      return null;
+    });
+  }
+
+  defaultReply(context) {
+    switch (context) {
+      case 'error':
+        return {
+          messages: [{
+            type: 'text',
+            text: 'I\'m sorry, something went wrong. Please try again!'
+          }]
+        };
+      case 'weather':
+        return {
+          messages: [{
+            type: 'text',
+            text: 'I\m sorry, I had trouble finding the weather there. Perhaps try a different location'
+          }]
+        }
+      default:
+        return {
+          messages: [{
+            type: 'text',
+            text: 'I\'m sorry, I don\'t know how to help with that. Try asking me something like "What\'s the weather in Chicago?"'
+          }]
+        };
+    }
   }
 }
 
@@ -91,5 +109,6 @@ module.exports = Bot;
 Potential improvements:
 - Remember users, so can give customized responses / remember locations
 - Multi-message context (ex: "What's the weather" > "Where?" > "In Chicago")
-- Better language processin
+- Better/more-robust language processing
+- More varied replies (esp. default replies)
 */
